a476b97a7ba6ba8de35cba06191b2f495bf6842c
RamReddymanda <ramreddymandha87@gmail.com>
2025-08-04 16:12:35 +0530

    corrected

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL ,
     methods: ['GET', 'POST'],
     credentials: true,
   },
@@ -21,7 +21,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
